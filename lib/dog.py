a0c1dc739fac4c234528a29ef9752ef82e01225d
Shania Lopez <slopez@fandm.edu>
2024-05-04 12:30:14 -0400

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python3
-
-class Dog:
+class Dog():
     def bark(self):
         print("Woof!")
     def sit(self):
         print("The dog is sitting.")
-
-fido = Dog()
-fido.bark()
-
-snoopy = Dog()
-snoopy.bark()
